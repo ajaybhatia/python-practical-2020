0aac26e761515fee163d4bfe5c336f60340180df
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-09-03 08:43:37 +0530

    first commit

---
+++ 01-practical.py
@@ -0,0 +1,27 @@
+'''
+Practical 1
+
+Compute sum, subtraction, multiplication, division and exponent of given variables
+input by the user.
+'''
+
+x = int(input("Enter first number : "))
+y = int(input("Enter second number: "))
+
+# Sum
+print(f"{x} + {y} = {x+y}")
+
+# Subtraction
+print(f"{x} - {y} = {x-y}")
+
+# Multiplication
+print(f"{x} X {y} = {x*y}")
+
+# Division
+print(f"{x} / {y} = {x/y}")
+
+# Integer Division
+print(f"{x} // {y} = {x//y}")
+
+# Exponentiation
+print(f"{x} ** {y} = {x**y}")
