bbbcfc56ad0f0454f30ebc7ea37883691b13519a
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-10-05 11:06:02 +0530

    Practical 26

---
+++ 26-practical.py
@@ -0,0 +1,21 @@
+'''
+Practical 26
+
+Maintain practical note book as per their serial numbers in library using Python
+dictionary.
+'''
+
+book = {
+    1: "Compute sum, subtraction, multiplication, division and exponent of given variables input by the user.",
+    2: "Compute area of following shapes: circle, rectangle, triangle, square, trapezoid and parallelogram.",
+    3: "Compute volume of following 3D shapes: cube, cylinder, cone and sphere.",
+    4: "Compute and print roots of quadratic equation ax 2 +bx+c=0, where the values of a, b, and c are input by the user.",
+    5: "Print numbers up to N which are not divisible by 3, 6, 9,, e.g., 1, 2, 4, 5, 7,....",
+    6: "Write a program to determine whether a triangle is isosceles or not?",
+    7: "Print multiplication table of a number input by the user.",
+    8: "Compute sum of natural numbers from one to n number.",
+    9: "Print Fibonacci series up to n numbers e.g. 0 1 1 2 3 5 8 13.....n",
+    10: "Compute factorial of a given number."
+}
+
+print(book[5])
