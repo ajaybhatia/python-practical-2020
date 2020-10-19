9bcfa8d69505677654d6431fac28c75f1bc43a40
ajaybhatia <prof.ajaybhatia@gmail.com>
2020-10-19 10:50:38 +0530

    Practical 34

---
+++ 34-practical.py
@@ -0,0 +1,12 @@
+'''
+Practical 34
+
+Write a program to create a module of factorial in Python.
+'''
+
+
+from factorial import fact
+
+number = int(input("Enter a number: "))
+
+print(f"Factorial of {number} is {fact(number)}")
